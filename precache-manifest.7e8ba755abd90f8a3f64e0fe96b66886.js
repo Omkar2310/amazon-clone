70b454a3e77ea9aada6153a934f82d15cfedf05e
Omkar2310 <mankar.omkar2310@gmail.com>
2021-01-17 16:11:49 +0530

    Updates

---
+++ precache-manifest.7e8ba755abd90f8a3f64e0fe96b66886.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81f56a60815e92cd370698996bdb921b",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "9f4e8de803955649ecb9",
+    "url": "/amazon-clone/static/css/main.e0050fdd.chunk.css"
+  },
+  {
+    "revision": "3e15961d8651313af612",
+    "url": "/amazon-clone/static/js/2.c133b10c.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/amazon-clone/static/js/2.c133b10c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f4e8de803955649ecb9",
+    "url": "/amazon-clone/static/js/main.aee9a056.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  }
+]);
